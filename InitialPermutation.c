383997872e81d0377d366f3eda46f142aaa172ee
sonhojin <thsghwls301@naver.com>
2017-03-31 13:24:30 +0900

    원래 ip와 iip도 잘된다.... 이유는 모르겠다.

--- InitialPermutation.c
+++ InitialPermutation.c
@@ -16,37 +16,32 @@ BIT IP[] = {58, 50, 42, 34, 26, 18, 10, 2,
 		61, 53, 45, 37, 29, 21, 13, 5,
 		63, 55, 47, 39, 31, 23, 15, 7};
 
-void InitialPermutation (WORD* data) {
-	int i, j;
-	WORD newdata[2] = {0};
-	WORD buf;
-	WORD tempMask,mask; //temp 뜻 : 임시직원
+void InitialPermutation(WORD *Data)
+{
+	int i;
+	WORD tmpMask;
 	WORD setMask;
-	for(i = 0 ; i <32 ; i++) {
-		tempMask = 0x00000001;
-		setMask = 0x00000001<<(31-i); //비트연산자를 통해 최우측비트가 한자리수가 높아지면(1씩증가하면) 2진수 한자리씩 가져오게된다.
-
-		//		동작부분
-		if((IP[i]<= 32) && (data[1] & setMask))
-			newdata[1] |= (tempMask << (32-IP[i]));
-		else if((IP[i]>32 && (data[1] & setMask)))
-			newdata[0] |= (tempMask << (64-IP[i]));
+	WORD NewData[2]= {0, };
+	for(i = 31; i >= 0; i--)
+	{
+		tmpMask = 0x00000001;
+		setMask = 0x00000001<<(31-i);
+		if((IP[i] <= 32) && (Data[1] & (tmpMask << (32-IP[i])))) {
+			NewData[1] = NewData[1] | setMask;
+		}else if((IP[i] > 32) && (Data[0] & (tmpMask << (64-IP[i])))){
+			NewData[1] = NewData[1] | setMask;
+		}
 	}
-	//		동작부분
-	for(i = 32 ; i < 64 ; i++) {
-		tempMask = 0x00000001;
-		setMask = 0x00000001<<(63-i); //비트연산자를 통해 최우측비트가 한자리수가 높아지면(1씩증가하면) 2진수 한자리씩 가져오게된다.
-
-		if((IP[i]<= 32) && (data[0] & setMask))
-			newdata[1] |=(tempMask << (32-IP[i]));
-		else if((IP[i]>32 && (data[0] & setMask)))
-			newdata[0] |=(tempMask << (64-IP[i]));
-
+	for(i = 63 ; i >=32 ; i--)
+	{
+		tmpMask = 0x00000001;
+		setMask = 0x00000001<<(63-i);
+		if((IP[i] <= 32) && (Data[1] & (tmpMask << (32-IP[i])))) {
+			NewData[0] = NewData[0] | setMask;
+		}else if((IP[i] > 32) && (Data[0] & (tmpMask << (64-IP[i])))) {
+			NewData[0] = NewData[0] | setMask;
+		}
 	}
-	data[1]=newdata[1];		//left   32-bit
-	data[0]=newdata[0];		//right 32-bit
-	newdata[1]=0;
-	newdata[0]=0;
-	puts(">>>>IP 완료! <<<<");
-
+	Data[1] = NewData[1];
+	Data[0] = NewData[0];
 }
